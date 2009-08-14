edc16dffd4e5b7ebb6ea27e5ad7603409738bf14
crezol <crezol@eqdkp-plus.eu>
2009-08-14 00:57:08 +0000

    add korean lang files.

---
+++ language/korean.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Project:     EQdkp-Plus
+ * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:       2008
+ * Date:        $Date: 2008-12-28 06:10:24 +0900 (2008-12-28, 일) $
+ * -----------------------------------------------------------------------
+ * @author      $Author: wallenium $
+ * @copyright   2006-2008 Corgan - Stefan Knaak | Wallenium & the EQdkp-Plus Developer Team
+ * @link        http://eqdkp-plus.com
+ * @package     eqdkp-plus
+ * @version     $Rev: 3531 $
+ * 
+ * $Id: english.php 3531 2008-12-27 21:10:24Z wallenium $
+ */
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');exit;
+}
+
+$plang = array_merge($plang, array(
+  'latestposts'                 => '최근 포럼 포스트',
+  'portal_latestposts_nomodule' => '모듈이 선택되지 않았습니다. 모듈 설정을 확인하세요!',
+  'portal_latestposts_invmodule'=> '잘못된 BB 모듈',
+  'pk_latestposts_bbmodule'     => '포럼 모듈',
+  'pk_latestposts_amount'       => '최근 x 개의 엔트리를 보입니다.',
+  'pk_latestposts_newdb'        => '다른 데이터베이스 내 BB ',
+  'pk_latestposts_dbname'       => '데이터베이스',
+  'pk_latestposts_dbuser'       => '사용자',
+  'pk_latestposts_dbpassword'   => '패스워드',
+  'pk_latestposts_dbhost'       => '호스트',
+  'pk_latestposts_dbprefix'     => '접두사',
+  'pk_latestposts_url'          => '게시판 URL',
+  'pk_latestposts_noentries'    => '보여질 내용이 없습니다.',
+  'pk_latestposts_connerror'    => '연결 오류',
+  'pk_latestposts_lastpost'     => '최근 게시물',
+  'pk_latestposts_starter'      => '게시자',
+  'pk_latestposts_posts'        => '답글',
+  'pk_latestposts_title'        => '제목',
+  'pk_latestposts_trimtitle'    => 'x 자를 넘는 긴 제목을 줄입니다.',
+  'pk_latestposts_trimtitle_h'  => '왼쪽과 오른쪽 모듈 블럭에 위치할 때만 작동합니다.',
+  'pk_latestposts_privateforums'=> '사설 포럼의 ID, 세미콜론으로 구분.',
+  'pk_latestposts_privforums_h' => '사설 포럼은 최근 포스트에서 보여지지 않습니다.',
+  'pk_latestposts_plus2old'     => 'EQdkp Plus 버전이 너무 오래되었습니다. 0.6.2.0 이상 버전이 필요합니다.',
+  'pk_latestposts_newwindow'    => '새 창에서 열까요?',
+  'pk_latestposts_blackwhite'   => '차단 - 허용리스트',
+  'pk_latestposts_blackwhite_h' => '거절할 포럼 ID 나 허용할 ID 를 입력하세요',
+));
+?>
