f037f6f1a92a131f654c72b4d4a6dab45231111d
corgan <corgan@eqdkp-plus.eu>
2010-07-09 16:07:05 +0000

    french & spanish translation

---
+++ language/spanish.php
@@ -0,0 +1,41 @@
+<?php
+
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
+}
+
+//Language: Spanish	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 15:10
+//File: module_lastestposts
+//Source-Language: english
+
+$alang = array( 
+"latestposts" => "Últimas entradas del foro",
+"portal_latestposts_nomodule" => "No hay módulo seleccionado. ¡Mira los ajustes del módulo!",
+"portal_latestposts_invmodule" => "Módulo BB erróneo",
+"pk_latestposts_bbmodule" => "Módulo de Foro",
+"pk_latestposts_amount" => "Mostrar las últimas x entradas",
+"pk_latestposts_newdb" => "BB está en una base de datos distinta",
+"pk_latestposts_dbname" => "Base de Datos",
+"pk_latestposts_dbuser" => "Usuario",
+"pk_latestposts_dbpassword" => "Contraseña",
+"pk_latestposts_dbhost" => "Host",
+"pk_latestposts_dbprefix" => "Prefijo",
+"pk_latestposts_url" => "URL del foro",
+"pk_latestposts_noentries" => "No hay entradas disponibles",
+"pk_latestposts_connerror" => "Error de conexión",
+"pk_latestposts_lastpost" => "Última entrada",
+"pk_latestposts_starter" => "Inicial",
+"pk_latestposts_posts" => "Respuestas",
+"pk_latestposts_title" => "Título",
+"pk_latestposts_trimtitle" => "Cortar títulos largos si superan x caracteres",
+"pk_latestposts_trimtitle_h" => "Sólo en los bloques de módulo de izquierda/derecha",
+"pk_latestposts_privateforums" => "ID de los foros privados, separados por punto y coma (;)",
+"pk_latestposts_privforums_h" => "Los foros privados no serán listados en las últimas entradas.",
+"pk_latestposts_plus2old" => "Versión de Plus demasiado antigua. Se necesita la versión 0.6.2.0 o superior",
+"pk_latestposts_newwindow" => "¿Abrir los enlaces de temas en nueva ventana?",
+"pk_latestposts_blackwhite" => "Listas blancas o negras",
+"pk_latestposts_blackwhite_h" => "Descartar los ID de foros insertados (lista negra) o aceptarlos (lista blanca)",
+ );
+$plang = array_merge($plang, $alang);
+?>
