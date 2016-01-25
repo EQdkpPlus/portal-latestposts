e1ba47e6ad4a4a652796d70bc29dbc10842337ec
GodMod <GodMod@users.noreply.github.com>
2016-01-25 18:49:12 +0100

    Bump Version

--- latestposts_portal.class.php
+++ latestposts_portal.class.php
@@ -28,7 +28,7 @@ class latestposts_portal extends portal_generic {
 	protected static $path		= 'latestposts';
 	protected static $data		= array(
 		'name'			=> 'Latest Forum Posts',
-		'version'		=> '3.1.1',
+		'version'		=> '3.1.2',
 		'author'		=> 'GodMod',
 		'icon'			=> 'fa-group',
 		'contact'		=> EQDKP_PROJECT_URL,
